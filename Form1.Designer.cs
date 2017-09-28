f95c740e72ca6e984c655fccbb122cef1cc7ffe7
Juan Sebastian Rocco <elbaterojuan@gmail.com>
2017-09-28 08:02:04 -0300

    Primer commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Gastos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 7;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.00972F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.2138F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.78717F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 11.66181F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.232265F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 38.09524F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 107F));
+            this.tableLayoutPanel1.Controls.Add(this.dateTimePicker1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.numericUpDown1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox2, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.checkBox1, 4, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 5, 0);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 6, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1138, 107);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "dd/MM/yyyy";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(3, 3);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(97, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.Value = new System.DateTime(2017, 9, 27, 11, 31, 7, 0);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(106, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(202, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Lara",
+            "Juan"});
+            this.comboBox2.Location = new System.Drawing.Point(425, 3);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(114, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(545, 3);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(85, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Proporcional";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(640, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(386, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1032, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 2;
+            this.numericUpDown1.Location = new System.Drawing.Point(314, 3);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            999999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(105, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1138, 107);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using System.Threading;
+using System.Globalization;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Gastos
+{
+    public partial class Form1 : Form
+    {
+        Microsoft.Office.Interop.Excel.Application oXL;
+        Workbooks oWBs;
+        Workbook oWB;
+
+        public Form1()
+        {
+            oXL = getExcel();
+            oWBs = oXL.Workbooks;
+            oXL.WindowState = XlWindowState.xlMaximized;
+            oXL.Visible = true;
+            oWB = getWorkbook();
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("es-ES");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("es-ES");
+            InitializeComponent();
+            dateTimePicker1.Value = DateTime.Now;
+            string[] strArray = new string[Properties.Settings.Default.Categorias.Count];
+            Properties.Settings.Default.Categorias.CopyTo(strArray, 0);
+            comboBox1.Items.AddRange(strArray);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Worksheet oSheet = getSheet(oWB, dateTimePicker1.Value);
+            oSheet.Select(Type.Missing);
+            addValues(oSheet, getLastRow(oSheet));
+            cleanInputs();
+        }
+
+
+        private int getLastRow(Worksheet oSheet)
+        {
+            for (int i = 1; i <= oSheet.UsedRange.Rows.Count; i++)
+            {
+                if (oSheet.Cells[i, 1].Value == null && oSheet.Cells[i, 2].Value == null && oSheet.Cells[i, 3].Value == null)
+                {
+                    return i;
+                }
+            }
+
+            return oSheet.UsedRange.Rows.Count;
+        }
+
+        private void addValues(Worksheet oSheet,int row)
+        {
+            oSheet.Cells[row, 1].Value = dateTimePicker1.Value.ToString("M/d/yyyy");
+            oSheet.Cells[row, 2].Value = comboBox1.Text;
+            oSheet.Cells[row, 3].Value = numericUpDown1.Value;
+            oSheet.Cells[row, 4].Value = comboBox2.Text;
+            oSheet.Cells[row, 5].Value = (checkBox1.Checked) ? "SI" : "NO";
+            oSheet.Cells[row, 8].Value = textBox1.Text;
+        }
+
+        private void cleanInputs()
+        {
+            comboBox1.Text = null;
+            numericUpDown1.Value = 1;
+            comboBox2.Text = null;
+            checkBox1.Checked = true;
+            textBox1.Text = null;
+        }
+
+        private Worksheet getSheet(Workbook oWB,DateTime date)
+        {
+            string month = date.ToString("MMMM", Thread.CurrentThread.CurrentCulture);
+            month = char.ToUpper(month[0]) + month.Substring(1);
+            return oWB.Worksheets[String.Format("{0}-{1}",month,date.ToString("yy"))];
+        }
+
+        private Microsoft.Office.Interop.Excel.Application getExcel()
+        {
+            try
+            {
+                return (Microsoft.Office.Interop.Excel.Application)Marshal.GetActiveObject("Excel.Application");
+            }
+            catch
+            {
+                return new Microsoft.Office.Interop.Excel.Application();
+            }
+            
+        }
+
+        private Workbook getWorkbook()
+        {
+            try
+            {
+                return oWBs.get_Item(Properties.Settings.Default.Archivo);
+            }
+            catch
+            {
+                return oWBs.Open(Path.Combine(Properties.Settings.Default.Carpeta,Properties.Settings.Default.Archivo));
+            }
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (oXL != null)
+            {
+                oWBs.Close();
+                oXL.Quit();
+
+                Marshal.ReleaseComObject(oWBs);
+                Marshal.ReleaseComObject(oXL);
+            }
+        }
+    }
+}
